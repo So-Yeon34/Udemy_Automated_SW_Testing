43f690d4fdc5539c07890f0612657b30a3f4f28a
Soyeon Kim <pooh361043@gmail.com>
2025-09-01 15:26:14 +0900

    python import

---
+++ import/code.py
@@ -0,0 +1,7 @@
+from mymodule import divide
+import sys
+# import mymodule
+# mymodule.divide(,)
+
+print(divide(15,3))
+print(sys.path) #check the path of "mymodule"
---
+++ import/mymodule.py
@@ -0,0 +1,4 @@
+def divide(devidend, divisor):
+    return devidend/divisor
+
+print("mymodule.py: ", __name__) #__name__ means global variable
