7be4dce5721ef96ee4abf8ce0d39ffab0b16b5ae
Soyeon Kim <pooh361043@gmail.com>
2025-09-02 10:40:43 +0900

    chore: remove imports folder from repo (keep local files)

--- import/code.py
+++
@@ -1,7 +0,0 @@
-from mymodule import divide
-import sys
-# import mymodule
-# mymodule.divide(,)
-
-print(divide(15,3))
-print(sys.path) #check the path of "mymodule"
--- import/mymodule.py
+++
@@ -1,4 +0,0 @@
-def divide(devidend, divisor):
-    return devidend/divisor
-
-print("mymodule.py: ", __name__) #__name__ means global variable
