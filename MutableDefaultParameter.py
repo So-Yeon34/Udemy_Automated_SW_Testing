9118608de18910309b14f798ddbf3a6f9a6a1bce
Soyeon Kim <pooh361043@gmail.com>
2025-09-03 10:58:42 +0900

    Add MutableDefaultParameter.py

---
+++ MutableDefaultParameter.py
@@ -0,0 +1,28 @@
+from typing import List, Optional
+
+'''
+class Student:
+    def __init__(self, name: str, grades: List[int]=[]): #This is bad!
+        self.name = name
+        self.grades = grades
+
+    def take_exam(self, result: int):
+        self.grades.append(result)
+'''
+class Student:
+    def __init__(self, name: str, grades: Optional[List[int]] = None):
+        self.name = name
+        self.grades = grades or []
+
+    def take_exam(self, result: int):
+        self.grades.append(result)
+
+bob = Student("Bob")
+rolf = Student("Rolf")
+bob.take_exam(90)
+
+print(bob.grades)
+print(rolf.grades)
+
+
+
