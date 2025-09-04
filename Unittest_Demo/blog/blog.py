87890226689dbfe5df28925be93cf4638a0c61f2
Soyeon Kim <pooh361043@gmail.com>
2025-09-04 14:16:43 +0900

    TDD, add integration test

--- Unittest_Demo/blog/blog.py
+++ Unittest_Demo/blog/blog.py
@@ -1,3 +1,9 @@
+'''
+TDD (Test Driven Development)
+Red -> Green -> Refactor
+'''
+from post import Post
+
 class Blog:
     def __init__(self, title, author):
         self.title = title
@@ -5,10 +11,18 @@ class Blog:
         self.posts = [] # Empty list
 
     def __repr__(self):
-        pass
-
+        return '{} by {} ({} post{})'.format(self.title, 
+                                            self.author, 
+                                            len(self.posts),
+                                            's' if len(self.posts) != 1 else '')
+    
     def create_post(self, title, content):
-        pass
+        self.posts.append(Post(title, content))
 
     def json(self):
-        pass
+        return {
+            'title': self.title,
+            'author': self.author,
+            'posts': [post.json() for post in self.posts]
+
+        }
